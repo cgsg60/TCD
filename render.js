0cf92f72f6cb0f7fbdd8b3b635cae85cdc496ebe
Artemy Arsentev <infaav@gmail.com>
2025-06-17 16:43:44 +0300

    dirt

--- render.js
+++ render.js
@@ -1,89 +1,131 @@
-let tcd;
+let stc, din;
 let startTime;
-let x = 0;
+let x = 0, y = 0;
+let ground, ava;
 
-let IsRight = IsLeft = false;
+let IsRight = IsLeft = IsUp = IsDown = false;
 
-function initGFX(canvas) {
-  tcd = canvas.getContext("2d");
+function isColliding(obj) {
+  let pixels = din.getImageData(80, 80, 50, 50);
+
+  for (let i = 3; i < pixels.data.length; i += 4) {
+    if (pixels[i] > 0) {
+      return true;
+    }
+  }
+  return false; 
+  // try {
+  //   const imageData = din.getImageData(0, 0, 500, 500);
+  //   console.log(imageData.data);
+  // } catch (e) {
+  //   console.error('Ошибка доступа к данным:', e.message);
+  //   // Альтернативная логика обработки
+  // }
+}
+
+function initGFX(canvas, dincanvas) {
+  stc = canvas.getContext("2d");
+  din = dincanvas.getContext("2d");
 
   const textureImage = new Image();
   textureImage.src = 'background.jpg';
 
   textureImage.onload = () => {
     let img = document.getElementById("background");
-    tcd.drawImage(img, 0, 0, 1900, 900);
+    stc.drawImage(img, 0, 0, 1900, 900);
+    
+    ground = document.getElementById("land1");
+    ground.crossOrigin = "Anonymous"
+    din.drawImage(ground, 0, 0, 1900, 900);  
   };
 }
 
-function drawScene(canvas) {
+function drawScene(canvas, dincanvas) {
   timeFromStart = new Date().getTime() - startTime;
 
-  tcd.clearRect(0, 0, canvas.width, canvas.height);
+  stc.clearRect(0, 0, canvas.width, canvas.height);
+  din.clearRect(0, 0, canvas.width, canvas.height);
 
   //background
   let img = document.getElementById("background");
-  tcd.drawImage(img, 0, 0, 1900, 900);
+  stc.drawImage(img, 0, 0, 1900, 900);
 
-  tcd.fillStyle = 'Blue';
-//  tcd.fillRect(x, 100, 50, 50);  
+  //stc.drawImage(dincanvas, 0, 0);
 
-  img = document.getElementById("avatar");
+  ava = document.getElementById("avatar");
+  stc.drawImage(ava, x + 200, y + 500, 50, 50);
 
-  tcd.beginPath();
-  tcd.arc(x, 100, 25, 0, Math.PI * 2);
-  tcd.closePath();
-  tcd.clip();
-  tcd.drawImage(img, x - 25, 100 - 25, 50, 50);
+/*
+  stc.beginPath();
+  stc.arc(x, 100, 25, 0, Math.PI * 2);
+  stc.closePath();
+  stc.clip();
+  stc.drawImage(img, x - 25, 100 - 25, 50, 50);
+*/
+  /*stc.beginPath();
+  stc.arc(150, 150, 100, 0, Math.PI * 2);
+  stc.fillStyle = 'black';
+  stc.fill();
+  stc.globalCompositeOperation = 'source-in';
 
-  /*tcd.beginPath();
-  tcd.arc(150, 150, 100, 0, Math.PI * 2);
-  tcd.fillStyle = 'black';
-  tcd.fill();
-  tcd.globalCompositeOperation = 'source-in';
-
-  tcd.globalCompositeOperation = 'source-over';*/
+  stc.globalCompositeOperation = 'source-over';*/
   
+  if (isColliding(ava))
+    console.log("colliding!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
   if (IsRight)
     x += 2;
   if (IsLeft)
     x -= 2;
+  if (IsDown)
+    y += 2;
+  if (IsUp)
+    y -= 2;
 
   window.requestAnimationFrame(drawScene);
 }
 
 function onStart() {
-  let canvas = document.getElementById("tcd-canvas");
+  let canvas = document.getElementById("static-canvas");
   canvas.width = 1900;
   canvas.height = 900;
-
+  let dincanvas = document.getElementById("dinamic-canvas");
+  dincanvas.width = canvas.width;
+  dincanvas.height = canvas.height;
+ 
   window.onkeydown = (event) => {
     if (event.code == "KeyA")
       IsLeft = true;
     else if (event.code == "KeyS")
-      console.log("S");
+      IsDown = true;
     else if (event.code == "KeyD")
       IsRight = true;
     else if (event.code == "Space")
-      console.log("space");    
+      IsUp = true;    
   };
 
   window.onkeyup = (event) => {
     if (event.code == "KeyA")
       IsLeft = false;
     else if (event.code == "KeyS")
-      console.log("S");
+      IsDown = false;  
     else if (event.code == "KeyD")
       IsRight = false;
     else if (event.code == "Space")
-      console.log("space");    
+      IsUp = false;    
   };
+  window.onmousedown = (event) => {
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const radius = 50;
+
+    din.clearRect(event.x - 26, event.y - 26, 52, 52);
+  }
 
 
-  initGFX(canvas);
+  initGFX(canvas, dincanvas);
 
   startTime = new Date().getTime();
 
-  drawScene(canvas);
+  drawScene(canvas, dincanvas);
 }
