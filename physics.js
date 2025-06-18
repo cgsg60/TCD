6d2bfb4c9fd8d0ae10a4e6d0af97466c213547db
Vov4iK44 <vova.nechaev.spb@gmail.com>
2025-06-18 15:08:52 +0300

    add some changes in physics.js

--- physics.js
+++ physics.js
@@ -1,9 +1,9 @@
 import { input } from "./input.js";
 import { stc } from "./render.js"
-
+import { timer } from "./render.js"
 let input_cont;
 
-const g = 9.8195;
+const g = -9.8195 * 30;
 
 class vector {
   constructor(x, y) {
@@ -13,15 +13,15 @@ class vector {
 }
 
 function sumvectors(V1, V2) {
-  return vector(V1.x + V2.x, V1.y + V2.y);
+  return new vector(V1.x + V2.x, V1.y + V2.y);
 }
 
 function subvectors(V1, V2) {
-  return vector(V1.x - V2.x, V1.y - V2.y);
+  return new vector(V1.x - V2.x, V1.y - V2.y);
 }
 
 function multvector(V1, a) {
-  return vector(V1.x * a, V1.y * a);
+  return new vector(V1.x * a, V1.y * a);
 }
 
 function lenvector(V1) {
@@ -31,7 +31,7 @@ function lenvector(V1) {
 function isColliding(obj, Land, avgpoint) {
   let x = Math.round(obj.x), y = Math.round(obj.y), w = Math.round(obj.width), h = Math.round(obj.height);
 
-  if (x == undefined || y == undefined || width <= 0 || height <= 0) {
+  if (x == undefined || y == undefined || w <= 0 || h <= 0) {
     console.error("underfined variables in function 'isColliding'");
   }
 
@@ -55,24 +55,28 @@ export function physics_init() {
   input_cont = new input(stc);
 }
 
+let velocity = 0;
+
 export function simulation(obj_1, obj_2, land) {
   let avgV = new vector(0, 0)
   let R1 = new vector(obj_1.x + (obj_1.width - obj_1.x) * 0.5, obj_1.y + (obj_1.height - obj_1.y) * 0.5);
-  let velocity = 0;
 
-  obj_1.x -= input_cont.keys['A'];
-  obj_1.x += input_cont.keys['D'];
+  obj_1.x -= input_cont.keys['KeyA'];
+  obj_1.x += input_cont.keys['KeyD'];
+  obj_1.y -= input_cont.keys['KeyW'];
+  obj_1.y += input_cont.keys['KeyS'];
 
-  if (input_cont.keysClick['Space'] == 1) {
-    velocity += 2.5 * g * globalDeltaTime;
-  }
 
   if (isColliding(obj_1, land, avgV)) {
-    let v = subvectors(R1, avgV)
+    let v = multvector(subvectors(R1, avgV), 1.0 / lenvector(subvectors(R1, avgV)))
+    velocity = 0;
+    obj_1.x -= v.x, obj_1.y -= v.y
+    if (input_cont.keysClick['Space'] == 1) {
+      velocity -= 50 * g * timer.globalDeltaTime;
+    }
 
-    obj_1.x += v.x, obj_1.y += v.y
   }
-
-  velocity += g * globalDeltaTime;
-  obj_1.y += velocity * globalDeltaTime;
+  console.log(obj_1.x, obj_1.y)
+  velocity += g * timer.globalDeltaTime;
+  obj_1.y -= velocity * timer.globalDeltaTime;
 }
